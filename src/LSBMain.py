7a8ccba63d3f63de0cb16d1f97b3beee9f38f1ac
Mohr <mahmoud.elmohr@gmail.com>
2016-12-11 10:58:34 +0200

    Enabled data hiding in image

--- src/LSBMain.py
+++ src/LSBMain.py
@@ -10,7 +10,7 @@ def divideData(image,rawData,numberBits):
 	#print(len(rawData))
 	#print(len(image))
 	div = int(math.ceil(len(rawData) / (len(image)*numberBits*3.0)))
-	print(div)
+	#print(div)
 	blockedData = [[[0 for x in range(3)] for y in range(len(image))] for z in range(div)] 
 	i=0	
 	for index in range(div):
@@ -31,13 +31,13 @@ def divideData(image,rawData,numberBits):
 def mergeData(image,data,numberBits):
 	if(numberBits > 8):
 		print "Error: Number of bits should not be larger than 8"
-	if(len(data)>(len(image)*numberBits*3)):
+	if(len(data)>(len(image))):
 		print "Error: Too much data to hide!!"
 	for index in range(0,len(image)):
 		tempPixel = list(image[index]) # convert to list to be able to change it
-		tempPixel[0] = (tempPixel[0] & 0xFF << numberBits) | 1 #1 should be replaced with data
-		tempPixel[1] = (tempPixel[1] & 0xFF << numberBits) | 2
-		tempPixel[2] = (tempPixel[2] & 0xFF << numberBits) | 3
+		tempPixel[0] = (tempPixel[0] & 0xFF << numberBits) | int(data[index][0],2)
+		tempPixel[1] = (tempPixel[1] & 0xFF << numberBits) | int(data[index][1],2)
+		tempPixel[2] = (tempPixel[2] & 0xFF << numberBits) | int(data[index][2],2)
 		image[index] = tuple(tempPixel) #right the data
 	return image
 
@@ -54,11 +54,11 @@ def getData(image,numberBits):
 
 imageMat = Image.open("./../samples/sample1.png") #read the image, 8 bit per pixel
 pixels = list(imageMat.getdata()) #the content of images (list of tupels)
-inputData=b'0000000011111111000000001111111100000000'
+inputData=b'0000000011111111000000001111111100000000' #Should be driven from a string to binay function
 blockedData=divideData(pixels,inputData,2)
-print bin(pixels[30000][0]) + "  "  + bin(pixels[30000][1]) + "  "+ bin(pixels[30000][2]) #checking before
-imageHidden =  mergeData(pixels,[1, 1, 1],2) #call the function that merges the data 
-print bin(pixels[30000][0]) + "  "  + bin(pixels[30000][1]) + "  "+ bin(pixels[30000][2]) #checking after 
+print bin(pixels[0][0]) + "  "  + bin(pixels[0][1]) + "  "+ bin(pixels[0][2]) #checking before
+imageHidden =  mergeData(pixels,blockedData[0],2) #call the function that merges the data 
+print bin(pixels[0][0]) + "  "  + bin(pixels[0][1]) + "  "+ bin(pixels[0][2]) #checking after 
 imageData = getData(imageHidden,2)
 #print imageData
 
